1e4b23e375dcf8c6c464b62afbda7726ab6c0d21
Pedro Barros <pedrobars7a@gmail.com>
2021-04-01 17:00:14 -0300

    first commit

--- src/config/database.js
+++ src/config/database.js
@@ -1,3 +1,6 @@
 const mongoose = require('mongoose')
 mongoose.Promise =  global.Promise
-module.exports =  mongoose.connect('mongodb://localhost/todo')
+const url = process.env.MONGODB_URI ? process.env.MONGODB_URI : 'mongodb://localhost/todo'
+// module.exports =  mongoose.connect('mongodb://localhost/todo')
+// module.exports =  mongoose.connect('mongodb+srv://todo:demolayg1@cluster0.xzkuh.mongodb.net/myFirstDatabase?retryWrites=true&w=majority')
+module.exports = mongoose.connect(url, {useNewUrlParser: true, useUnifiedTopology: true})
--- src/config/server.js
+++ src/config/server.js
@@ -1,4 +1,5 @@
-const port = 3003
+// const port = 3003
+const port = process.env.PORT
 
 const bodyParser = require('body-parser')
 const express = require('express')
